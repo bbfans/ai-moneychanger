9db036572ae39517dd3f934bb47e3d3898c294cb
Jie Chen <jiechen2013@outlook.com>
2025-04-26 20:35:44 +0000

    Call LLM and Use Developer Token

--- ai-moneychanger.py
+++ ai-moneychanger.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 load_dotenv()
 EXCHANGERATE_API_KEY = os.getenv("EXCHANGERATE_API_KEY")
 
-token = os.environ["GITHUB_TOKEN"]
+token = os.getenv("GITHUB_TOKEN")
 endpoint = "https://models.github.ai/inference"
 model_name = "openai/gpt-4o-mini"
 
@@ -29,49 +29,90 @@ def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
-    try:
-        response = client.chat.completions.create(messages=[{
-            "role":
-            "system",
-            "content":
-            "You are a helpful assistant.",
-        }, {
-            "role":
-            "user",
-            "content":
-            textbox_input,
-        }],
-                                                  temperature=1.0,
-                                                  top_p=1.0,
-                                                  max_tokens=1000,
-                                                  model=model_name)
+    tools = [{
+        "type": "function",
+        "function": {
+            "name": "exchange_rate_function",
+            "description":
+            "Convert a given amount of money from one currency to another. Each currency will be represented as a 3-letter code",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "base": {
+                        "type": "string",
+                        "description": "The base or original currency.",
+                    },
+                    "target": {
+                        "type": "string",
+                        "description": "The target or converted currency",
+                    },
+                    "amount": {
+                        "type":
+                        "string",
+                        "description":
+                        "The amount of money to convert from the base currency.",
+                    },
+                },
+                "required": ["base", "target", "amount"],
+                "additionalProperties": False,
+            },
+        },
+    }]
 
-        print(response.choices[0].message.content)
+    try:
+        response = client.chat.completions.create(
+            messages=[{
+                "role": "system",
+                "content": "You are a helpful assistant.",
+            }, {
+                "role": "user",
+                "content": textbox_input,
+            }],
+            temperature=1.0,
+            top_p=1.0,
+            max_tokens=1000,
+            model=model_name,
+            tools=tools,
+        )
+
     except Exception as e:
         print(f"Exception {e} for {text}")
     else:
-        return completion
+        return response  #.choices[0].message.content
 
 
-# def run_pipeline():
-#     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
-#     Call get_exchange_rate(...) if necessary"""
+def run_pipeline():
+    """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
+    Call get_exchange_rate(...) if necessary"""
 
-#     if True:  #tool_calls
-#         # Update this
-#         st.write(
-#             f'{base} {amount} is {target} {exchange_response["conversion_result"]:.2f}'
-#         )
+    response = call_llm(user_input)
 
-#     elif True:  #tools not used
-#         # Update this
-#         st.write(f"(Function calling not used) and response from the model")
-#     else:
-#         st.write("NotImplemented")
+    #st.write(response)
+
+    if response.choices[0].finish_reason == "tool_calls":  #tool_calls
+        # Update this
+        response_arguments = json.loads(
+            response.choices[0].message.tool_calls[0].function.arguments)
+        ## {"amount":"100","base":"USD","target":"CAD"}
+        base = response_arguments["base"]
+        target = response_arguments["target"]
+        amount = response_arguments["amount"]
+        _, _, _, conversion_result = get_exchange_rate(base, target, amount)
+        st.write(f'{base} {amount} is {target} {conversion_result}')
+
+    elif True:  #tools not used
+        # Update this
+        st.write(
+            f"(Function calling not used) and {response.choices[0].message.content}"
+        )
+    else:
+        st.write("NotImplemented")
+
 
 st.title("AI Money Changer")
 
 user_input = st.text_input("Enter your prompt here")
 
 if st.button("Submit"):
-    st.write(call_llm(user_input))
+    # st.write(call_llm(user_input))
+    run_pipeline()
